e3f8285174e85d951fde6647f4f0e7bad4a80220
SivaK-98 <sivasubramaniyan.k@flipkart.com>
2022-03-25 10:37:27 +0530

    third commit

--- Flask/main.py
+++ Flask/main.py
@@ -17,67 +17,6 @@ time = str(datetime.now())
 def index():
     return render_template("home.html")
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    print(username)
-    print(password)
-    connection = sqlite3.connect(DataBase)
-    cur = connection.cursor()
-    try:
-        db_fetch_name = cur.execute(f"select * from '{user_table}' where username = '{username}' ;").fetchone()[1]
-        print(db_fetch_name)
-        my_password = cur.execute(f"select PASSWORD from '{user_table}' where username = '{username}';").fetchone()[0]
-        db_password = my_password.encode()
-        key = cur.execute(f" select KEY from '{user_table}' where username = '{username}';").fetchone()[0]
-        f = Fernet(key)
-        decMessage = f.decrypt(db_password).decode()
-        print("Password: ",decMessage)
-        connection.close()
-        print("User Exists! Proceeding with password validation")
-        result = "User Exists! Proceeding with password validation"
-        if password == decMessage:
-            print("Login Success")
-            result = "Login Success"
-        else:
-            print("Please check the password you mentioned")
-            result = "Please check the password you Entered"
-
-    except TypeError:
-        print("No Matchign records found...")
-        print("User does not exixts")
-        result = "User does not exists"
-
-        
-
-    return render_template("dbcreds.html")
-
-@app.route('/add_entry', methods=['POST', 'GET'])
-def add_entry():
-    connection = sqlite3.connect(DataBase)
-    cur = connection.cursor()
-    component_name = request.form.get('name')
-    password = request.form.get('password')
-    db_count = cur.execute(f"select count(*) from '{creds_table}';").fetchone()[0]
-    connection.commit()
-    connection.close()
-    dbpass = password.encode()
-    key = Fernet.generate_key()
-    f = Fernet(key)
-    id = db_count + 1
-    encrypted = f.encrypt(dbpass)
-    data_tuple = (id,component_name,encrypted,key,time)
-    query = f"insert into '{creds_table}' (id, component, password, key, created) values(?,?,?,?,?);"
-    try:
-        connection = sqlite3.connect(DataBase)
-        cur = connection.cursor()
-        cur.execute(query, data_tuple)
-        connection.commit()
-        connection.close()
-    except sqlite3.Error as er:
-        result = "Error"
-    return render_template("dbcreds.html")
 
 @app.route('/signup_page', methods=['POST','GET'])
 def signup_page():
@@ -126,6 +65,78 @@ def signup():
         result = "Error"
 
     return render_template("home.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    global login_username
+    login_username = request.form.get("username")
+    password = request.form.get("password")
+    print(login_username)
+    print(password)
+    connection = sqlite3.connect(DataBase)
+    cur = connection.cursor()
+    try:
+        db_fetch_name = cur.execute(f"select * from '{user_table}' where username = '{login_username}' ;").fetchone()[1]
+        print(db_fetch_name)
+        my_password = cur.execute(f"select PASSWORD from '{user_table}' where username = '{login_username}';").fetchone()[0]
+        db_password = my_password.encode()
+        key = cur.execute(f" select KEY from '{user_table}' where username = '{login_username}';").fetchone()[0]
+        f = Fernet(key)
+        decMessage = f.decrypt(db_password).decode()
+        print("Password: ",decMessage)
+        connection.close()
+        print("User Exists! Proceeding with password validation")
+        result = "User Exists! Proceeding with password validation"
+        if password == decMessage:
+            print("Login Success")
+            result = "Login Success"
+            return render_template("dbcreds.html")
+        else:
+            print("Please check the password you mentioned")
+            result = "Please check the password you Entered"
+            return result
+
+    except TypeError:
+        print("No Matchign records found...")
+        print("User does not exixts")
+        result = "User does not exists"
+        return result
+
+        
+
+    
+
+@app.route('/add_entry', methods=['POST', 'GET'])
+def add_entry():
+    
+    username = login.login_username
+    print("component page username: ",username)
+    connection = sqlite3.connect(DataBase)
+    cur = connection.cursor()
+    component_name = request.form.get('name')
+    print("component name:",component_name)
+    password = request.form.get('password')
+    print("component password: ",password)
+    db_count = cur.execute(f"select count(*) from '{creds_table}';").fetchone()[0]
+    connection.commit()
+    connection.close()
+    dbpass = password.encode()
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    id = db_count + 1
+    encrypted = f.encrypt(dbpass)
+    data_tuple = (id,username,component_name,encrypted,key,time)
+    query = f"insert into '{creds_table}' (id, username,component, password, key, created) values(?,?,?,?,?,?);"
+    try:
+        connection = sqlite3.connect(DataBase)
+        cur = connection.cursor()
+        cur.execute(query, data_tuple)
+        connection.commit()
+        connection.close()
+    except sqlite3.Error as er:
+        result = "Error"
+    return render_template("dbcreds.html")
+
 
 
 if __name__ == "__main__":
